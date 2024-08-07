0ef3a1078f1b4e8f2ef1bfd861b9f21c42b58db5
Sky.xu <770970190@qq.com>
2024-08-07 10:55:26 +0800

    只是用来测试学习使用的git所创建的代码文件

---
+++ test_git.c
@@ -0,0 +1,22 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: sky.xu
+ * @Date: 2024-08-07 10:32:24
+ * @LastEditTime: 2024-08-07 10:37:41
+ * @FilePath: \data_structure\test_git.c
+ */
+#include <stdio.h>
+void sort(int arr[],int size)
+{
+    
+
+}
+int main()
+{
+    int arr[] = {11,12,45,1,45,54,8,4};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    sort(arr,size);
+    return 0;
+
+}
