5787d680dcdd57ef3e9304d428aead736691d5f1
Sky.hou <1808035720@qq.com>
2024-08-07 12:07:27 +0800

    完善了function的代码

--- test_git.c
+++ test_git.c
@@ -9,7 +9,21 @@
 #include <stdio.h>
 void sort(int arr[],int size)
 {
-    
+   int tmp  = 0;
+   for(int i = 0; i < size - 1; ++i)
+   {
+   for (int j = 0; i < size - 1; ++j )
+   {
+   	if(arr[j] > arr[j+1])
+	{
+	tmp = arr[j];
+	arr[j] = arr[j + 1];
+	arr[j + 1] = tmp;
+	
+	}
+   }
+   
+   }
 
 }
 int main()
