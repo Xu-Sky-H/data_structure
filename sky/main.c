1b9681dd33470a6e0a834aa32e58d782af3f0e05
Sky.xu <770970190@qq.com>
2024-08-09 11:15:32 +0800

    这是一个测试

---
+++ sky/main.c
@@ -0,0 +1,13 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: sky.xu
+ * @Date: 2024-08-09 11:13:36
+ * @LastEditTime: 2024-08-09 11:13:55
+ * @FilePath: \Git_Sky_code\data_structure\sky\main.c
+ */
+#include <stdio.h>
+int main()
+{
+    printf("这是一个测试文件");
+}
